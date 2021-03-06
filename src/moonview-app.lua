bf1efd9d95c070dc8985a94f477a7187e693c291
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2021-03-05 22:39:28 -0400

    Update Identation

--- src/moonview-app.lua
+++ src/moonview-app.lua
@@ -5,17 +5,17 @@
  @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com>
  @date      03.03.2021 18:05:16 -04
 ]]
-local screen    = Gdk.Screen:get_default()
+local screen	   = Gdk.Screen:get_default()
 
-local main_window   = Gtk.Window ({
-    default_width = screen:get_width(),
-    default_height = screen:get_height(),
-    Gtk.ScrolledWindow {
-        Gtk.Image {
-            id      = 'image_view',
-            visible = true
-        }
-    }
+local main_window  = Gtk.Window ({
+	default_width  = screen:get_width(),
+	default_height = screen:get_height(),
+	Gtk.ScrolledWindow {
+		Gtk.Image {
+			id      = 'image_view',
+			visible = true
+		}
+	}
 })
 
 local headerbar = Gtk.HeaderBar {
@@ -28,9 +28,9 @@ main_window:set_titlebar(headerbar)
 function app:on_open(files)
 	file = files[1] and files[1]:get_parse_name()
 	if(file) then
-        image = GdkPixbuf.Pixbuf.new_from_file(file)
+		image = GdkPixbuf.Pixbuf.new_from_file(file)
 	end
-    main_window.child.image_view:set_from_pixbuf(image)
+	main_window.child.image_view:set_from_pixbuf(image)
 	main_window:show_all()
 end
 
@@ -41,5 +41,5 @@ end
 
 function app:on_activate()
 	main_window:show_all()
-    main_window:set_icon_name('image-viewer')
+	main_window:set_icon_name('image-viewer')
 end
